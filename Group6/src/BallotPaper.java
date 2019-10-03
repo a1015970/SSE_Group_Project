05aedf9d37fa3cb5ae88d9482c286fd647c16fc4
Chris Crouch <a1015970@student.adelaide.edu.au>
2019-10-03 10:37:17 +0930

    Initial, take 2

--- Group6/src/BallotPaper.java
+++ Group6/src/BallotPaper.java
@@ -1,4 +1,13 @@
+import java.util.List;
 
 public class BallotPaper {
-
+	public final List<Party> partyList;
+	public final List<Candidate> ungroupedCandidates;
+	
+	// interim constructor
+	// final version -read a file (CSV or JSON?) and create candidates and parties
+	public BallotPaper(List<Party> partyList, List<Candidate> ungroupedCandidates) {
+		this.partyList = partyList;
+		this.ungroupedCandidates = ungroupedCandidates;
+	}
 }
