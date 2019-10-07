40d46585e649fd731c6a3ce403056e68e27c136f
Chris Crouch <a1015970@student.adelaide.edu.au>
2019-10-07 14:21:53 +1030

    Experiments with PBKDF2 & AES

--- Group6/src/CryptoEngine.java
+++ Group6/src/CryptoEngine.java
@@ -1,3 +1,4 @@
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
@@ -14,6 +15,11 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class CryptoEngine {
 	
@@ -88,6 +94,7 @@ public class CryptoEngine {
 		return votePrivateKey;
 	}
 	
+	// generate random keys. not useful except for testing
 	static void generateNewVoteKeys() throws NoSuchAlgorithmException {
 		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 		keyPairGenerator.initialize(4096);
@@ -96,7 +103,7 @@ public class CryptoEngine {
 		votePublicKey = keyPair.getPublic();
 	}
 	
-	
+	// we don't want these hard-coded keys in the final system!
 	static void getStaticVoteKeys() throws NoSuchAlgorithmException, InvalidKeySpecException {
 		String encodedPublicKey = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAt43OaPV1Md2zLwZCW6WxUo1QPBaiJVr4bDUsIXoIAufDwHRpxphwI3oJOmX9D6t27B+/f+wWRwVLXY5QXeioEWAoF26u86YZHpusKywkKfC3XtdhPRVLXxYiP71TnrjXn7O9JSzk5F4/vz+qxCIfH569Ggft8HihJV7rVxDDmEJadUf0TMetDU8uPmeLBlkKSiIFJzauyfNOQsTc34eCln6J/pJcEsTUjjQ0NLnPByz1HTzUC/ah15y+Qe8MzdtzwG8n7H+vFWFdGB9hyUcj2VnP81vUZH0lhIIqTBMTsyW45xgsXlmLcAr7tJBE6Roko1nJlM7VlKQTOraNJprv/NirhLQqwBY3YE20cr0tQOPDo8168rNg8jc6sUFwAS+PV9EYf1ajD6jgTpapwIZ9rBuo0uh2M8aDzVfKi5kPBBOkFVoki/mjHNKwDfYa+93onaWkG7UWQbVlKoC8BTJ8/TW+vcfkosdjRtZ+HqJ1EfZKiZxtFA5SF6SryY2Hu2TVD3wNDZSXX5FncNoPr2j8LV4kFlzQBdlXfQ5bT4BEkB/cswtCVRWbJMUx2Eqlg/ucRbZ7Gz8xgmt446CdkP42r/uwuqF6w95nY3uNlZu51Wqd0U6IJTdWoOxKGTwZIIAK5KM05d8PXqnjMtPWe83GHy7aVFvApxj1kBaA8IKOvEcCAwEAAQ==";
 		String encodedPrivateKey = "MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQC3jc5o9XUx3bMvBkJbpbFSjVA8FqIlWvhsNSwheggC58PAdGnGmHAjegk6Zf0Pq3bsH79/7BZHBUtdjlBd6KgRYCgXbq7zphkem6wrLCQp8Lde12E9FUtfFiI/vVOeuNefs70lLOTkXj+/P6rEIh8fnr0aB+3weKElXutXEMOYQlp1R/RMx60NTy4+Z4sGWQpKIgUnNq7J805CxNzfh4KWfon+klwSxNSONDQ0uc8HLPUdPNQL9qHXnL5B7wzN23PAbyfsf68VYV0YH2HJRyPZWc/zW9RkfSWEgipMExOzJbjnGCxeWYtwCvu0kETpGiSjWcmUztWUpBM6to0mmu/82KuEtCrAFjdgTbRyvS1A48OjzXrys2DyNzqxQXABL49X0Rh/VqMPqOBOlqnAhn2sG6jS6HYzxoPNV8qLmQ8EE6QVWiSL+aMc0rAN9hr73eidpaQbtRZBtWUqgLwFMnz9Nb69x+Six2NG1n4eonUR9kqJnG0UDlIXpKvJjYe7ZNUPfA0NlJdfkWdw2g+vaPwtXiQWXNAF2Vd9DltPgESQH9yzC0JVFZskxTHYSqWD+5xFtnsbPzGCa3jjoJ2Q/jav+7C6oXrD3mdje42Vm7nVap3RToglN1ag7EoZPBkggArkozTl3w9eqeMy09Z7zcYfLtpUW8CnGPWQFoDwgo68RwIDAQABAoICAGpnavtPJzPUB/Y/k6IbCBMJ1jQvDqnG9XGM1VtGqXQ9tC1RjqvQXFlEeir/SlaWGCdDNDjMvBFJkWnXgOEfaKZB7pu+zu5xH+itZ33TZddmMnZpNnY5bOrfItV+RAYjsuNiR0hQoRN9S0jreGugOBiVZZu8cwNOemP2hUiAcFJITLXmE0mbCsdHWlBUp7PzU9Krq/8SZPqu73QnOL7fiCUDj60iXPuIXIte2dCi0c+gESt0wpt4ylL6CxSfpfWDvLMV/LfNQMqNGXFNZ9tNQQvTU4gIeLQUE/afmjNlb6sPOAPG8Jl1IS6/PzBp3XifbhO/jeqUiB1Vi8wiXOq/MFSOyLM656IXL7nrGZEgzz//eEa341Hw6eSFpIKWqN/tUJAJYAwEnFv9NWejQhvgfolLScUh3V8IiMzOFmUqQKzva7qo69PRP5UKVBKAg1g9e/o66ObBwoAKO1QNfFY6tPjD4EUr+iwLWCKm369GhkpdFvt8bwltP+9wMj+uleAaTQnHP4wtazsQOUCpum91cXTvn5fY+HxH3KwtJs3avyGCNRzE1LOoorUKZzUP/AyQMP14PQa1hJ2d1pxh6dNA4a55/cmp2S59ViLBBm20HxCr22irkNwzBh4z76FUdUXvrOBJaDrla4kaEK+OSm3hkraEsFeOk5nDI1QLpoUav/0RAoIBAQDyRasppJo8yAf0bW5xdpVtdRmyQY8/SoGeiY+L2/Lh6HHMqTpwc/bfR0zUTRgGDf/NjZAxKSBd7PtaFJb0mBBz/MN3KxNYHq3+t23+ImZZe7rUJlVpOWQYASK5JmXVrAPnfrWMQBhAtfFNRRxFdnEzfpYm2eKNp2E8g9MQnadEGBBaAbMaTQueg1WOeNy32QS4uKRGsE+DOLSxlPr2MIwFXCzfsMfLH04SEVwcBHfw8DnCMy8KY8XxUzFIRqE4DShvO9FZmjn/54zDFG4wRvHaBfqhxf9Lr/+ttm9AAwpnwJIRRurIAd4+MViSpz29bRFda4f4i6b7p4aBE5ZYPSjPAoIBAQDB9GNKeeCPKIsr1IJLlvd698tXDfQjvxKzDyiFTRG366lyJ/Qe/kJjBAqP2Hgn9S7j9lFSF9RRM35QWzpEL9pmqYJisQqgGzXUAsfljJa5gKbajboKmWiVWyWvoihtyEcwhtGyd05LYKvC0hJkgRJH5FjWFgqSnZhKuYcD3H78HuKnh9Yij1OHJ56rATjAzgXVuVh2jHZTXVJkzOL+M6hzpbvh9iBlQMq7Q+fqPWwabfYE4ZDv69DS/DvKzdXR0DxsjGPqgqZNRmt/W2YcRhD0mQVfVIWlxWqPyGkj32hwXqVbSL0h6YWH9sMrPM//icds+Gp1Vs/ICvLEelUD7CMJAoIBAQC0ubPNBLTp0m3csCVLDqK5XDkm+DDbjcDTetpSAmlUjAkYGsU1TcxDEvUFCd155seDlq6RfZRffICj2egh3a2oWE2Xf9KWUYyKDltG0HJ5HgtoZpAgRmecYHx+kV413nOrJKJZVRbCbvbqJTvJtWkMeMmZgb+22711XwD7zB8SdMD943avj/my7VXNSeucWY9kHJivAqbNxGdGcVQkgmJxPSlcVIs1wsyCPeDwJYUzfoAXlgbpQTRPqTeCKu3o2ifnkj/BD4Nkml4ux/bKnKGdeghU1VZ0J34bH5QttBb2/nf322cYfAxFaJoiDA/KVqo1fhmYFtUhtP5pGytz92uRAoIBAHFtcg/hHPG7/UABxL1eKZdRiSp9L5UkLlRnfgQPIViVSoBHW14wxjRP7blYGFUN74FD0SUYaIOggLOP1pCAB9LYOTdsYFFn+F/nzG2zfYY49duE3RNLyjzmlDCVvd0OLovznHkpirFMdMa3wifi0AUZRtbVBxPddG3m+E3Kyry5d0YQi6ukdG3rPsC1MzKWihUGkvSpip2QSxpGNE4DefVALJOmNCXi8Wz49o0npKwEOMd7/x5Ao9xvxKwDGpPcmynEJL7F72nrz/woQJwyYcIaJ2kr7gkXXg1+X/aptdNZloy/ClTl08DDEDqeEupX8jgJQ4Eh5twPgxBilot3srkCggEAUaphcS2cXKRgjUTvJBFg6EbgMznsGDXxFihYiR8JNEJH1sy+10dhGZmR4t6SizPVvHsJrXoOqDsl5owH7DxtiNq4xWE6ZeVqsF0YfFa+vYuzpKG/xnaNRJIDmTtdgj6hiuJRdcBJp29xVDp+blpnUdgZ8NnVUIWrEV02PAz6C+nMpHpaf0npGI0QkOhgFdhvbYPj/1EA3bQSWWUlMp4KCpwR11/NW0VGvuytJ3d/wIIx4W4aM76DpkLmdlaNym80E5hmLj2Ga5GLowYIwlf0AgZyndw4O/T/ANVuCbvbGkpsBqwlC0sSAEgYOxXE1C2i4MsAD98J71AUYtOYIqoyiA"; 
@@ -107,5 +114,33 @@ public class CryptoEngine {
 	    KeyFactory kfPriv = KeyFactory.getInstance("RSA");
 	    votePrivateKey =  kfPriv.generatePrivate(specPriv);
 	}
+	
+	
+	public static void main(String[] args) throws InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+		byte[] salt = {1,2,3,4,5,6,7,8,9,0};
+		int iterations = 100000;
+		long start = System.currentTimeMillis();
+        PBEKeySpec spec = new PBEKeySpec("password".toCharArray(), salt, iterations, 128);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        SecretKey key = skf.generateSecret(spec);
+        System.out.println("Key length: " + key.getEncoded().length + " bytes");
+        System.out.println("Key: " + Base64.getEncoder().encode(key.getEncoded()));
+        key = new SecretKeySpec(key.getEncoded(), "AES");
+        long end = System.currentTimeMillis();
+        System.out.println(iterations + " iterations took " + (end-start) + " ms");
+        
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(new byte[16]));
+        byte[] encrypted = c.doFinal("plaintext".getBytes());
+        System.out.println("Encrypted: " + encrypted.toString());
+        System.out.println("Encrypted: " + new String(encrypted));
+        System.out.println("Encrypted: " + Base64.getEncoder().encode(encrypted));
+        
+        Cipher d = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        d.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(new byte[16]));
+        byte[] decrypted = d.doFinal(encrypted);
+        System.out.println("Decrypted: " + new String(decrypted));
+	}
+	
 	
 }
